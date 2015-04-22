38e6d4dc7a0ffedad9daa58ad0182dc441cca825
Michael Granger <ged@FaerieMUD.org>
2015-04-22 09:04:07 -0700

    Checkpoint before trying out loom/originate

--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,75 @@
 // Karma configuration
-// Generated on Mon Mar 09 2015 15:08:56 GMT-0700 (PDT)
-
-// Karma configuration
+// Generated on Tue Apr 21 2015 17:57:48 GMT-0700 (PDT)
 
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jspm', 'jasmine'],
-    browsers: ['Chrome'],
-    reporters: ['progress', 'verbose', 'coverage'],
-    singleRun: true,
-    colors: true,
-    files: [],
-    jspm: {
-      // Edit this to your needs
-      config: 'src/system.config.js',
-      loadFiles: ['spec/**/*.js'],
-      serveFiles: [
-        'dist/**/*',
-        'jspm_packages/**/*.js',
-        'jspm_packages/**/*.css'
-      ]
-    },
+	config.set({
 
-    proxies: {
-      '/base/app': '/base/dist/app',
-      '/base/common': '/base/dist/common',
-      '/jspm_packages': '/base/jspm_packages'
-    },
+		basePath: '',
+		frameworks: ['jasmine', 'systemjs'],
+		// plugins: ['karma-systemjs'],
 
-    // list of files to exclude
-    exclude: [],
+		systemjs: {
+			// Path to your SystemJS configuration file
+			configFile: 'src/system.config.js',
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+			// File patterns for your application code, dependencies, and test suites
+			// list of files / patterns to load in the browser
+			files: [
+				'src/**/*.js',
+				'spec/**/*.spec.js'
+			],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'spec/**/*.js': ['babel', 'coverage']
-    },
+			// SystemJS configuration specifically for tests, added after your config file.
+			// Good for adding test libraries and mock modules
+			config: {
+				paths: {
+					// 'angular-mocks': 'bower_components/angular-mocks/angular-mocks.js'
+				}
+			},
 
-    // optionally, configure the reporter
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    }
+			// Specify the suffix used for test suite file names.  Defaults to 
+			// .test.js, .spec.js, _test.js, and _spec.js
+			testFileSuffix: '.spec.js'
+		},
 
-  });
+		// list of files to exclude
+		exclude: [],
+
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {},
+
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
+
+
+		// web server port
+		port: 9876,
+
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome', 'PhantomJS'],
+
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false
+	});
 };
