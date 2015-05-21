6b9b51e7cb5eb757130ec4d7ce46ea65939c957a
Michael Granger <ged@FaerieMUD.org>
2015-05-20 20:37:28 -0700

    Class-based matchers don't work

--- spec/helpers.js
+++ spec/helpers.js
@@ -1,95 +1,24 @@
 /* -*- javascript -*- */
 "use strict";
 
-import 'babel/polyfill';
+export var customMatchers = {
 
-class JasmineCustomMatcher {
+	toBeA: function( utils, customEqTesters ) {
+		return {
+			compare: function( actual, expected ) {
+				var result = {
+					pass: actual instanceof expected
+				};
+				if ( result.pass ) {
+					result.message = `${actual} is a ${expected.name}`;
+				} else {
+					result.message = `${actual} is not a ${expected.name}`;
+				}
 
-	constructor( utils, eqTesters ) {
-		this.utils = utils;
-		this.eqTesters = eqTesters;
-		this.actual = null;
-		this.expected = null;
-		this.verb = 'match';
-	}
-
-	matches() {
-		console.warn( "The default custom matcher never matches!" );
-		return false;
-	}
-
-	compare( actual, expected ) {
-		this.actual = actual;
-		this.expected = expected;
-
-		return this;
-	}
-
-	get pass() { return this.matches(); }
-
-	get message() {
-		if ( this.matches() ) {
-			return this.describePositiveMatch();
-		} else {
-			return this.describeNegativeMatch();
-		}
-	}
-
-	describePositiveMatch() {
-		return `${this.actual} does ${this.verb} ${this.expected}`
-	}
-
-	describeNegativeMatch() {
-		return `${this.actual} does not ${this.verb} ${this.expected}`;
-	}
-
-}
-
-
-class BeAMatcher extends JasmineCustomMatcher {
-	matches() {
-		return this.actual instanceof this.expected;
-	}
-
-	describePositiveMatch() {
-		return `${this.actual} is a ${this.expected.name}`;
-	}
-
-	describeNegativeMatch() {
-		return `${this.actual} is not a ${this.expected.name}`;
-	}
-}
-
-
-/*
- * Promise matchers
- */
-
-class BeFulfilledMatcher extends JasmineCustomMatcher {
-
-	matches() {
-		return this.actual.isFulfilled();
-	}
-
-	describePositiveMatch() {
-		return `${this.actual} is fulfilled.`
-	}
-
-	describeNegativeMatch() {
-		return `${this.actual} is not fulfilled.`
+				return result;
+			}
+		};
 	}
 
 };
-
-
-
-export class CustomMatcherFactory {
-
-	toBeA( ...args ) { return new BeAMatcher(...args); }
-	toBeFulfilled( ...args ) { return new BeFulfilledMatcher(...args); }
-
-}
-
-export var customMatchers = new CustomMatcherFactory();
-
 
