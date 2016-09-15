ed05bdcbf4af1f920078a7ba128c882a993260b3
Michael Granger <ged@FaerieMUD.org>
2016-09-14 20:50:09 -0700

    Add a "ci" test task for Semaphore

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -14,6 +14,17 @@ gulp.task('test', ['build'], function (done) {
 
 	return server.start();
 });
+gulp.task('ci', ['build'], function (done) {
+	var server = new karma.Server({
+		configFile: __dirname + '/../../karma.conf.js',
+		singleRun: true,
+		browsers: ['phantomjs']
+	}, function(e) {
+		done();
+	});
+
+	return server.start();
+});
 
 /**
 * Watch for file changes and re-run tests on each change
