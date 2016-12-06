9fbf609c6c8b8b8d94cb394ed2739f87ecc4ff6b
Michael Granger <ged@FaerieMUD.org>
2016-12-05 16:08:23 -0800

    Don't specify the format in package.json anymore

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Promise from 'bluebird';
  * The default namespace
  */
 
-export var VERSION = '0.0.1-alpha.5';
+export var VERSION = '0.0.1-alpha.6';
 
 export * from './model';
 export * from './datastore';
