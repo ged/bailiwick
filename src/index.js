7871b014de28c1fa1edde9d327c39dd4ceb3b6af
Michael Granger <ged@FaerieMUD.org>
2017-11-07 15:58:28 -0800

    Bump the prerelease version

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Promise from 'bluebird';
  * The default namespace
  */
 
-export var VERSION = '0.0.1-alpha.12';
+export var VERSION = '0.0.1-alpha.13';
 
 export * from './model';
 export * from './datastore';
